6cd74bb85c628e82d58cfd4e4a0e8bc3563ec4ab
BKWANGHO <bkwangho@naver.com>
2024-03-21 19:53:00 +0900

    khj

--- src/main/java/com/turing/api/board/Board.java
+++ src/main/java/com/turing/api/board/Board.java
@@ -1,22 +1,17 @@
 package com.turing.api.board;
 import java.util.List;
 
+import jakarta.persistence.*;
 import org.hibernate.annotations.ManyToAny;
 
 import com.turing.api.article.Article;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import lombok.*;
 
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @ToString(exclude = {"id"})
-@Entity
 public class Board {
 
     private long id;
