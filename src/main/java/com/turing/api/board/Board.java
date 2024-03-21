c91b9dff1500e22bea8cf5d8f78e4e4d3c896375
wawaHyun <sjrkchdkdy@gmail.com>
2024-03-21 18:55:14 +0900

    ,

--- src/main/java/com/turing/api/board/Board.java
+++ src/main/java/com/turing/api/board/Board.java
@@ -1,20 +1,39 @@
 package com.turing.api.board;
+import java.util.List;
+
+import org.hibernate.annotations.ManyToAny;
+
+import com.turing.api.article.Article;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.*;
 
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @ToString(exclude = {"id"})
+@Entity
 public class Board {
+
     private long id;
     private String boardName;
     private String boardType;
+
+    @OneToMany(mappedBy = "board")
+    @JoinColumn(name="ref_id",referencedColumnName = "id")
+    private List<Article> article;
 
     @Builder(builderMethodName = "builder")
     public Board(long id, String boardName, String boardType) {
         this.id = id;
         this.boardName = boardName;
         this.boardType = boardType;
-"백광호"
     }
+
+
+
 }
