b22ba2a6ebc7375b84fe2b6df656f1efbe28e68e
BKWANGHO <bkwangho@naver.com>
2024-03-29 18:31:09 +0900

    240329

--- src/main/java/com/turing/api/article/ArticleController.java
+++ src/main/java/com/turing/api/article/ArticleController.java
@@ -15,22 +15,22 @@ import java.util.*;
 public class ArticleController {
     private final ArticleService service;
 
-    @GetMapping("/api/articles")
+    @GetMapping("/api/all-articles")
     public Map<?, ?> findAll() {
         Map<String, Object> map = new HashMap<>();
 
-        map.put("messege", Messenger.SUCCESS);
 //        @SuppressWarnings("unchecked")
-        List<Article> list = new ArrayList<>();
-//        list.add(Article.builder()
-//                .id(1L)
-//                .title("타임스크립트")
-//                .content("너무 어렵다")
-//                .writer("배광호")
-//                .registerDate("24-03-27")
-//                .build());
-                        list = service.findAll();
+        List<Article> list = service.findAll();
+//        list = service.findAll();
+        list.add(Article.builder()
+                .id(1L)
+                .title("타임스크립트")
+                .content("너무 어렵다")
+                .registerDate("24-03-27")
+                .build());
+        map.put("messege", Messenger.SUCCESS);
         map.put("result", list);
+        System.out.println("리스트 몇개?"+list.size());
         return map;
     }
 
